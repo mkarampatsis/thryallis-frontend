6ff973eb9ad75762acffc5b3314fd63cbac95d94
Ubuntu <cloudadmin@MIN09RG14PVM01.0rgkt3djqtleta5k0a43cxkfka.ax.internal.cloudapp.net>
2024-11-11 11:19:31 +0000

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ypes-backend.ddns.net',
-    elasticUrl:' http://ypessearch.ddns.net:8080'
+    apiUrl: 'https://thryallis-backend.ypes.gov.gr',
+    elasticUrl: 'https://ypessearch.ddns.net'
 };
