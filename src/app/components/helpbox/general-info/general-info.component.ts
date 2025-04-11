b7b235817e0067843caf43eb7f9e9d8cb0fa044b
Markos Karampatsis <marka@central.ntua.gr>
2025-04-11 19:05:54 +0300

    helpbox

--- src/app/components/helpbox/general-info/general-info.component.ts
+++ src/app/components/helpbox/general-info/general-info.component.ts
@@ -13,204 +13,312 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { take } from 'rxjs';
 import { ModalService } from 'src/app/shared/services/modal.service';
 import { set } from 'lodash-es';
+import { GridApi, GridReadyEvent, ColDef } from 'ag-grid-community';
+import { AgGridAngular, ICellRendererAngularComp } from 'ag-grid-angular';
+import { GridLoadingOverlayComponent } from 'src/app/shared/modals/grid-loading-overlay/grid-loading-overlay.component';
+import { ConstService } from 'src/app/shared/services/const.service';
 
 @Component({
   selector: 'app-general-info',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, NgxEditorModule, NgbAlertModule],
+  imports: [CommonModule, ReactiveFormsModule, NgxEditorModule, NgbAlertModule, AgGridAngular, GridLoadingOverlayComponent],
   templateUrl: './general-info.component.html',
   styleUrl: './general-info.component.css'
 })
 export class GeneralInfoComponent {
-    authService = inject(AuthService);
-    uploadService = inject(FileUploadService);
-    helpboxService = inject(HelpboxService);
-    userService = inject(UserService);
-    modalService = inject(ModalService);
-    sanitizer = inject(DomSanitizer);
+  authService = inject(AuthService);
+  uploadService = inject(FileUploadService);
+  helpboxService = inject(HelpboxService);
+  userService = inject(UserService);
+  modalService = inject(ModalService);
+  sanitizer = inject(DomSanitizer);
+  constService = inject(ConstService);
 
-    user = this.authService.user;
-    editor: Editor = new Editor();
-    toolbar: Toolbar = DEFAULT_TOOLBAR;
+  user = this.authService.user;
+  editor: Editor = new Editor();
+  toolbar: Toolbar = DEFAULT_TOOLBAR;
 
-    progress = 0;
-    currentFile: File;
-    uploadObjectID: string | null = null;
+  progress = 0;
+  currentFile: File;
+  uploadObjectID: string | null = null;
 
-    generalInfo: IGeneralInfo[];
-    showGeneralInfo: IGeneralInfo[];
-    text: string;
-    // tags: string[] | null = [];
-    // selectedValues: string[] = [];
-    // showSelectedValues: string[] = [];
+  generalInfo: IGeneralInfo[];
+  showGeneralInfo: IGeneralInfo[];
+  text: string;
+  // tags: string[] | null = [];
+  // selectedValues: string[] = [];
+  // showSelectedValues: string[] = [];
 
-    form = new FormGroup({
-        email: new FormControl(''),
-        firstName: new FormControl(''),
-        lastName: new FormControl(''),
-        title: new FormControl('', Validators.required),
-        text: new FormControl('', Validators.required),
-        file: new FormControl(''),
-        category: new FormControl("")
-    });
+  form = new FormGroup({
+    email: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    title: new FormControl('', Validators.required),
+    text: new FormControl('', Validators.required),
+    file: new FormControl(''),
+    category: new FormControl("")
+  });
 
-    ngOnInit() {
-        this.getAllGeneralInfo();
-        this.initializeForm();
+  defaultColDef = {
+    resizable: true,
+    filter: true,
+    sortable: true,
+    floatingFilter: true,
+    wrapText: true, // Wrap Text
+    autoHeight: true,
+    wrapHeaderText: true, // Wrap Header Text
+    autoHeaderHeight: true,
+  };
+  
+  colDefs: ColDef[] = [
+    { 
+      headerName: 'A/A', 
+      cellRenderer : function (params) {
+          return params.rowIndex +1;
+      },
+      flex:1 
+    },
+    { 
+      field: 'title', 
+      headerName: 'Τίτλος',
+      cellRenderer: HtmlSanitizeRendererComponent,
+      flex:2
+    },
+    { 
+      field: 'text', 
+      headerName: 'Κείμενο',
+      cellRenderer: HtmlSanitizeRendererComponent,
+      flex:2
+    },
+    { 
+      field: 'category', 
+      headerName: 'Κατηγορία',
+      flex:1
+    },
+    { 
+      field: 'when.$date', 
+      headerName: 'Ημερ. ενημέρωσης', 
+      cellRenderer: (params) => {
+          const date = new Date(params.value);
+          const day = String(date.getDate()).padStart(2, '0');
+          const month = String(date.getMonth() + 1).padStart(2, '0');
+          const year = date.getFullYear();
+          return `${day}-${month}-${year}`;
+      }, 
+      flex:1 
+    },
+    { 
+      field: 'email', 
+      headerName: 'Χειριστής Helpdesk',
+      flex:1 
+    },
+    { 
+      field: 'file.$oid', 
+      headerName: 'Αρχείο',
+      flex:1 
+    },
+  ];
+
+  autoSizeStrategy = this.constService.autoSizeStrategy;
+
+  loadingOverlayComponent = GridLoadingOverlayComponent;
+  loadingOverlayComponentParams = { loadingMessage: 'Αναζήτηση ερωτημάτων...' };
+
+  gridApi: GridApi<any>;
+
+  ngOnInit() {
+      this.getAllGeneralInfo();
+      this.initializeForm();
+  }
+
+  ngOnDestroy(): void {
+      this.editor.destroy();
+  }
+
+  initializeForm(){
+      this.form.controls.email.patchValue(this.user().email);
+      this.form.controls.firstName.patchValue(this.user().firstName);
+      this.form.controls.lastName.patchValue(this.user().lastName);
+      this.form.controls.title.patchValue("");
+      this.form.controls.text.patchValue("");
+      this.form.controls.file.patchValue("");
+      this.form.controls.category.patchValue("");
+  }
+
+  onTextChange(html: object) {
+      this.text = html.toString()
+  }
+
+  onSubmit() {
+
+      // const inputValues = this.form.controls.tags.value.split(",").map(item => item.trim())
+      // this.selectedValues = this.selectedValues.concat(inputValues);
+          
+      const infoText = {
+          email: this.form.controls.email.value,
+          lastName: this.form.controls.lastName.value,
+          firstName: this.form.controls.firstName.value,
+          title: this.form.controls.title.value,
+          text: this.text,
+          file: this.uploadObjectID,
+          // tags: this.selectedValues
+          category: this.form.controls.category.value
+          
+      } as IGeneralInfo;
+
+      this.helpboxService.newGeneralInfo(infoText)
+              .subscribe(data => {
+                  // this.goToTab(1);
+                  this.initializeForm()
+                  this.getAllGeneralInfo();
+              });
+  }
+
+  onGridReady(params: GridReadyEvent<any>): void {
+    this.gridApi = params.api;
+    this.gridApi.showLoadingOverlay();
+    this.getAllGeneralInfo();;
+  }
+
+  getAllGeneralInfo(){
+      this.helpboxService.getGeneralInfo()
+          .subscribe((data)=>{
+              this.generalInfo = data;
+              this.showGeneralInfo = this.generalInfo;
+              console.log(this.showGeneralInfo);
+              // data.forEach(data => {
+              //     this.tags = this.tags.concat(data.tags);
+              // })
+              // this.tags = [...new Set(this.tags)];
+          })
+  }
+
+  resetForm(){
+    this.text = '';
+    this.initializeForm(); 
+  }
+
+  sanitizeHtml(html) : SafeHtml {
+    if (html) {
+        return this.sanitizer.bypassSecurityTrustHtml(html);
+    } else {
+        return ""
     }
+  }
 
-    ngOnDestroy(): void {
-        this.editor.destroy();
-    }
+  selectFile(event: any): void {
 
-    initializeForm(){
-        this.form.controls.email.patchValue(this.user().email);
-        this.form.controls.firstName.patchValue(this.user().firstName);
-        this.form.controls.lastName.patchValue(this.user().lastName);
-        this.form.controls.title.patchValue("");
-        this.form.controls.text.patchValue("");
-        this.form.controls.file.patchValue("");
-        this.form.controls.category.patchValue("");
-    }
+      if (event.target.files.length === 0) {
+          console.log('No file selected!');
+          return;
+      }
+      this.currentFile = event.target.files[0];
 
-    onTextChange(html: object) {
-        this.text = html.toString()
-    }
+      this.uploadService.upload(this.currentFile).subscribe({
+          next: (event: any) => {
+              if (event.type === HttpEventType.UploadProgress) {
+                  this.progress = Math.round((100 * event.loaded) / event.total);
+              } else if (event instanceof HttpResponse) {
+                  this.uploadObjectID = event.body.id;
+                  this.form.controls.file.setValue(this.uploadObjectID);
+                  this.form.markAsDirty();
+              }
+          },
+          error: (err: any) => {
+              console.log(err);
+          },
+          complete: () => {
+              console.log('Upload complete');
+          },
+      });
+  }
 
-    onSubmit() {
-
-        // const inputValues = this.form.controls.tags.value.split(",").map(item => item.trim())
-        // this.selectedValues = this.selectedValues.concat(inputValues);
-           
-        const infoText = {
-            email: this.form.controls.email.value,
-            lastName: this.form.controls.lastName.value,
-            firstName: this.form.controls.firstName.value,
-            title: this.form.controls.title.value,
-            text: this.text,
-            file: this.uploadObjectID,
-            // tags: this.selectedValues
-            category: this.form.controls.category.value
-           
-        } as IGeneralInfo;
-
-        this.helpboxService.newGeneralInfo(infoText)
-                .subscribe(data => {
-                    // this.goToTab(1);
-                    this.initializeForm()
-                    this.getAllGeneralInfo();
-                });
-    }
-
-    getAllGeneralInfo(){
-        this.helpboxService.getGeneralInfo()
-            .subscribe((data)=>{
-                this.generalInfo = data;
-                this.showGeneralInfo = this.generalInfo;
-                // data.forEach(data => {
-                //     this.tags = this.tags.concat(data.tags);
-                // })
-                // this.tags = [...new Set(this.tags)];
-            })
-    }
-
-    resetForm(){
-        this.text = '';
-        this.initializeForm(); 
-    }
-
-    sanitizeHtml(html) : SafeHtml {
-        if (html) {
-            return this.sanitizer.bypassSecurityTrustHtml(html);
-        } else {
-            return ""
-        }
-    }
-
-    selectFile(event: any): void {
-
-        if (event.target.files.length === 0) {
-            console.log('No file selected!');
-            return;
-        }
-        this.currentFile = event.target.files[0];
-
-        this.uploadService.upload(this.currentFile).subscribe({
-            next: (event: any) => {
-                if (event.type === HttpEventType.UploadProgress) {
-                    this.progress = Math.round((100 * event.loaded) / event.total);
-                } else if (event instanceof HttpResponse) {
-                    this.uploadObjectID = event.body.id;
-                    this.form.controls.file.setValue(this.uploadObjectID);
-                    this.form.markAsDirty();
+  displayFile(fileId:string) {
+      this.uploadService
+          .getUploadByID(fileId)
+          .pipe(take(1))
+          .subscribe((data) => {
+              if (data.type==="application/pdf") {
+                const url = window.URL.createObjectURL(data);
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = 'document.pdf';
+                this.modalService.showPdfViewer(link);
+              } else {
+                // const type = data.type.split('/')[1];
+                console.log(">>",data.type);
+                if (data.type==='image/png'){
+                  console.log("1>>",data.type);
+                  this.helpboxService.downloadFile(data, 'image.png', 'image/png');
                 }
-            },
-            error: (err: any) => {
-                console.log(err);
-            },
-            complete: () => {
-                console.log('Upload complete');
-            },
-        });
-    }
-
-    displayFile(fileId:string) {
-        this.uploadService
-            .getUploadByID(fileId)
-            .pipe(take(1))
-            .subscribe((data) => {
-                if (data.type==="application/pdf") {
-                  const url = window.URL.createObjectURL(data);
-                  const link = document.createElement('a');
-                  link.href = url;
-                  link.download = 'document.pdf';
-                  this.modalService.showPdfViewer(link);
-                } else {
-                  // const type = data.type.split('/')[1];
-                  console.log(data.type);
-                  if (data.type=='image/png')
-                    this.helpboxService.downloadFile("imageBinaryData", 'image.png', 'image/png');
-                  if (data.type=='image/jpeg')
-                    this.helpboxService.downloadFile("imageBinaryData", 'photo.jpg', 'image/png');
-                  if (data.type=='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-                    this.helpboxService.downloadFile("xlsxBinaryData", 'sheet.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-                  if (data.type=='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-                    this.helpboxService.downloadFile("xlsxBinaryData", 'sheet.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-                  this.helpboxService.downloadFile("docxBinaryData", 'document.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-                  // const url = window.URL.createObjectURL(data);
-                  // const link = document.createElement('a');
-                  // link.href = url;
-                  // link.download = 'document.'+type;
-                  // console.log(link.download);
+                if (data.type=='image/jpeg'){
+                  console.log("2>>",data.type);
+                  this.helpboxService.downloadFile(data, 'photo.jpg', 'image/png');
                 }
-            });
-    }
+                if (data.type=='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'){
+                  console.log("3>>",data.type);
+                  this.helpboxService.downloadFile(data, 'sheet.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                }
+                if (data.type=='application/vnd.openxmlformats-officedocument.wordprocessingml.document'){
+                  console.log("4>>",data.type);
+                  this.helpboxService.downloadFile(data, 'document.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+                }
+              }
+          });
+  }
 
-    // onCheckboxChange(event:Event){
-    //     const checkbox = event.target as HTMLInputElement;
-    //     const value = checkbox.value;
+  // onCheckboxChange(event:Event){
+  //     const checkbox = event.target as HTMLInputElement;
+  //     const value = checkbox.value;
 
-    //     if (checkbox.checked) {
-    //         // Add value to selectedValues if checked
-    //         this.selectedValues.push(value);
-    //     } else {
-    //         // Remove value from selectedValues if unchecked
-    //         this.selectedValues = this.selectedValues.filter((item) => item !== value);
-    //     }
-    // }
+  //     if (checkbox.checked) {
+  //         // Add value to selectedValues if checked
+  //         this.selectedValues.push(value);
+  //     } else {
+  //         // Remove value from selectedValues if unchecked
+  //         this.selectedValues = this.selectedValues.filter((item) => item !== value);
+  //     }
+  // }
 
-    onSelectionChange(event:Event){
-      const selectection = event.target as HTMLInputElement;
-      const value = selectection.value;
-      this.showGeneralInfo = this.generalInfo;
-      this.showGeneralInfo = this.generalInfo.filter((item)=>item.category.includes(value));
-    }
+  onSelectionChange(event:Event){
+    const selectection = event.target as HTMLInputElement;
+    const value = selectection.value;
+    this.showGeneralInfo = this.generalInfo;
+    this.showGeneralInfo = this.generalInfo.filter((item)=>item.category.includes(value));
+  }
 
-    hasHelpDeskRole() {
-        return this.userService.hasHelpDeskRole();
-    }
-    
-    hasEditorRole() {
-        return this.userService.hasEditorRole();
-    }
+  hasHelpDeskRole() {
+      return this.userService.hasHelpDeskRole();
+  }
+  
+  hasEditorRole() {
+      return this.userService.hasEditorRole();
+  }
+
+  onRowClicked(event: any): void {
+    this.modalService.showFaqAnswer(event.data);
+    console.log(event);
+  }
+}
+
+@Component({
+  selector: 'app-html-sanitize-renderer',
+  template: `<div [innerHTML]="sanitizedHtml"></div>`
+})
+export class HtmlSanitizeRendererComponent implements ICellRendererAngularComp {
+  sanitizedHtml: SafeHtml = '';
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  agInit(params: any): void {
+    this.sanitizedHtml = this.sanitizeHtml(params.value);
+  }
+
+  refresh(): boolean {
+    return false;
+  }
+
+  sanitizeHtml(html: string): SafeHtml {
+    return html ? this.sanitizer.bypassSecurityTrustHtml(html) : '';
+  }
 }
--- src/app/components/helpbox/helpdesk/helpdesk.component.ts
+++ src/app/components/helpbox/helpdesk/helpdesk.component.ts
@@ -1,8 +1,7 @@
 import { Component, inject, effect } from '@angular/core';
-import { GridApi, GridReadyEvent } from 'ag-grid-community';
+import { GridApi, GridReadyEvent, ColDef } from 'ag-grid-community';
 import { AgGridAngular } from 'ag-grid-angular';
 import { GridLoadingOverlayComponent } from 'src/app/shared/modals/grid-loading-overlay/grid-loading-overlay.component';
-import { ColDef } from 'ag-grid-community';
 import { ModalService } from 'src/app/shared/services/modal.service';
 import { IHelpbox } from 'src/app/shared/interfaces/helpbox/helpbox.interface';
 import { HelpboxService } from 'src/app/shared/services/helpbox.service';
@@ -26,73 +25,73 @@ export class HelpdeskComponent {
 
     defaultColDef = this.constService.defaultColDef;
     colDefs: ColDef[] = [
-        { field: 'key', headerName: 'Κωδικός Ερώτησης', flex:1 },
-        { field: 'questionTitle', headerName: 'Τίτλος Συνομιλίας', flex:1 },
-        { field: 'questionCategory', headerName: 'Κατηγορία', flex:1 },
-        // { field: 'email', headerName: 'Χρήστης', flex:1 },
-        { field: 'firstName', headerName: 'Όνομα', flex:1 },
-        { field: 'lastName', headerName: 'Επίθετο', flex:1 },
-        {
-            field: 'organizations',
-            headerName: 'Φορείς',
-            cellRenderer: (params) => {
-                const organizationPreferedLabel = [];
-                params.value.forEach((data) => {
-                    organizationPreferedLabel.push(this.constService.getOrganizationPrefferedLabelByCode(data));
-                });
-                return organizationPreferedLabel.join(', ');
-            }, 
-            unSortIcon: true,
-            filter: false,
-            flex:1
+      { field: 'key', headerName: 'Κωδικός Ερώτησης', flex:1 },
+      { field: 'questionTitle', headerName: 'Τίτλος Συνομιλίας', flex:1 },
+      { field: 'questionCategory', headerName: 'Κατηγορία', flex:1 },
+      // { field: 'email', headerName: 'Χρήστης', flex:1 },
+      { field: 'firstName', headerName: 'Όνομα', flex:1 },
+      { field: 'lastName', headerName: 'Επίθετο', flex:1 },
+      {
+        field: 'organizations',
+        headerName: 'Φορείς',
+        cellRenderer: (params) => {
+          const organizationPreferedLabel = [];
+          params.value.forEach((data) => {
+              organizationPreferedLabel.push(this.constService.getOrganizationPrefferedLabelByCode(data));
+          });
+          return organizationPreferedLabel.join(', ');
+        }, 
+        unSortIcon: true,
+        filter: false,
+        flex:1
+      },
+      // { 
+      //     field: 'when.$date', 
+      //     headerName: 'Ημερομηνία', 
+      //     cellRenderer: function (params) {
+      //         return (new Date(params.value)).toLocaleDateString();
+      //     }
+      //     , flex:1 
+      // },
+      { field: 'toWhom', headerName: 'Χειριστής Helpdesk', flex:1},
+      { 
+        field: 'questions', 
+        headerName: 'Ερωτήσεις/Απαντήσεις', 
+        cellRenderer: function (params) {
+            let answered = params.value.filter(value => value.answered === true);
+            // let checker = params.value.every(value => value.answered === true);
+            // console.log(checker);
+            // return params.value ? "Απαντήθηκε" : 'Εκκρεμεί';
+            return params.value.length + "/" + answered.length;
         },
-        // { 
-        //     field: 'when.$date', 
-        //     headerName: 'Ημερομηνία', 
-        //     cellRenderer: function (params) {
-        //         return (new Date(params.value)).toLocaleDateString();
-        //     }
-        //     , flex:1 
-        // },
-        { field: 'toWhom', headerName: 'Χειριστής Helpdesk', flex:1},
-        { 
-            field: 'questions', 
-            headerName: 'Ερωτήσεις/Απαντήσεις', 
-            cellRenderer: function (params) {
-                let answered = params.value.filter(value => value.answered === true);
-                // let checker = params.value.every(value => value.answered === true);
-                // console.log(checker);
-                // return params.value ? "Απαντήθηκε" : 'Εκκρεμεί';
-                return params.value.length + "/" + answered.length;
-            },
-            cellStyle: params => {
-                let checker = params.value.every(value => value.answered === true);
-                // if (params.value) {
-                if (checker) {
-                    return {color: 'green'};
-                } else {
-                    return {color: 'red'};
-                }
-            }, 
-            flex:1
+        cellStyle: params => {
+            let checker = params.value.every(value => value.answered === true);
+            // if (params.value) {
+            if (checker) {
+                return {color: 'green'};
+            } else {
+                return {color: 'red'};
+            }
+        }, 
+        flex:1
+      },
+      { 
+        field: 'finalized', 
+        headerName: 'Κατάσταση συνομιλίας', 
+        cellRenderer: function (params) {
+            return params.value ? "Ολοκληρώθηκε" : 'Ανοιχτή';
         },
-        { 
-            field: 'finalized', 
-            headerName: 'Κατάσταση συνομιλίας', 
-            cellRenderer: function (params) {
-                return params.value ? "Ολοκληρώθηκε" : 'Ανοιχτή';
-            },
-            cellStyle: params => {
-                if (params.value) {
-                    return {color: 'green'};
-                } else {
-                    return {color: 'red'};
-                }
-            }, 
-            unSortIcon: true,
-            filter: false,
-            flex:1
-        },
+        cellStyle: params => {
+            if (params.value) {
+                return {color: 'green'};
+            } else {
+                return {color: 'red'};
+            }
+        }, 
+        unSortIcon: true,
+        filter: false,
+        flex:1
+      },
     ];
 
     autoSizeStrategy = this.constService.autoSizeStrategy;
@@ -103,37 +102,37 @@ export class HelpdeskComponent {
     gridApi: GridApi<IHelpbox>;
 
     constructor() {
-        effect(
-            () => {
-                if (this.helpboxService.helpboxNeedUpdate()){
-                    this.loadData();
-                }
-                this.helpboxService.helpboxNeedUpdate.set(false)
-            },
-            { allowSignalWrites: true }
-        );
+      effect(
+        () => {
+          if (this.helpboxService.helpboxNeedUpdate()){
+            this.loadData();
+          }
+          this.helpboxService.helpboxNeedUpdate.set(false)
+        },
+        { allowSignalWrites: true }
+      );
     }
 
     onGridReady(params: GridReadyEvent<IHelpbox>): void {
-        this.gridApi = params.api;
-        this.gridApi.showLoadingOverlay();
-        this.loadData();
+      this.gridApi = params.api;
+      this.gridApi.showLoadingOverlay();
+      this.loadData();
     }
 
     loadData(){
-        this.helpboxService.getAllHelpbox().subscribe((helpbox) => {
-            if (this.userService.hasEditorRole()) {
-                this.helpbox = helpbox.filter(item => item.email===this.userService.user().email);
-            } else {
-                this.helpbox = helpbox;
-            }
-            // console.log(">>>",this.helpbox)
-            this.gridApi.hideOverlay();
-        });
+      this.helpboxService.getAllHelpbox().subscribe((helpbox) => {
+        if (this.userService.hasEditorRole()) {
+          this.helpbox = helpbox.filter(item => item.email===this.userService.user().email);
+        } else {
+          this.helpbox = helpbox;
+        }
+        // console.log(">>>",this.helpbox)
+        this.gridApi.hideOverlay();
+      });
     }
 
     onRowClicked(event: any): void {
-        this.modalService.showHelpboxAnswer(event.data._id.$oid);
+      this.modalService.showHelpboxAnswer(event.data._id.$oid);
     }
 
 }
