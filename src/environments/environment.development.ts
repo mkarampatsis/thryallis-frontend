eca4d16c1f05d8c8b2c7bba45871b9769ea24970
mkarampatsis <marka@central.ntua.gr>
2025-09-26 06:49:14 +0300

    GSIS

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,9 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000',
-    elasticUrl:'https://ypessearch.ddns.net',
-    logoutUrl: 'https://www1.gsis.gr/oauth2servergov/logout',
-    gsisClientId:'LSZSWH27387',
-    redirectUri: 'https://thryallis.ypes.gov.gr/login'
+  production: false,
+  apiUrl: 'http://localhost:5000',
+  elasticUrl: 'https://ypessearch.ddns.net',
+  logoutUrl: 'https://www1.gsis.gr/oauth2servergov/logout',
+  gsisClientId: 'LSZSWH27387',
+  redirectUri: 'https://thryallis.ypes.gov.gr/login',
+  enableGoogleAuth: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 import { getVersionSuffix } from "ngx-extended-pdf-viewer";
 
 export const environment = {
-    production: true,
-     //apiUrl: 'https://ypes-backend.ddns.net',
-    apiUrl: 'https://thryallis-backend.ypes.gov.gr',
-    elasticUrl:' https://ypessearch.ddns.net',
-    logoutUrl: 'https://www1.gsis.gr/oauth2servergov/logout',
-    gsisClientId:'LSZSWH27387',
-    redirectUri: 'https://thryallis.ypes.gov.gr/login'
+  production: true,
+  //apiUrl: 'https://ypes-backend.ddns.net',
+  apiUrl: 'https://thryallis-backend.ypes.gov.gr',
+  elasticUrl: ' https://ypessearch.ddns.net',
+  logoutUrl: 'https://www1.gsis.gr/oauth2servergov/logout',
+  gsisClientId: 'LSZSWH27387',
+  redirectUri: 'https://thryallis.ypes.gov.gr/login',
+  enableGoogleAuth: true 
 };
