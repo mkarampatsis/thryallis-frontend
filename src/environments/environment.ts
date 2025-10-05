65945608a6e46fba7416240ad3a56f72a48e3630
mkarampatsis <marka@central.ntua.gr>
2025-10-05 19:31:31 +0300

    Configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import { getVersionSuffix } from "ngx-extended-pdf-viewer";
 
 export const environment = {
   production: true,
-  apiUrl: 'https://147.102.152.103/backend',
+  apiUrl: 'https://thryallis-backend.duckdns.org',
   //apiUrl: 'https://thryallis-backend.ypes.gov.gr',
   elasticUrl: ' https://ypessearch.ddns.net',
   logoutUrl: 'https://www1.gsis.gr/oauth2servergov/logout',
