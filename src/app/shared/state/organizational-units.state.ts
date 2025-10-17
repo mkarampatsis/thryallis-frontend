938993c44890951f8cd7e3ef321fec14a94f8081
mkarampatsis <marka@central.ntua.gr>
2025-10-17 13:21:41 +0300

    changes to state

--- src/app/shared/state/organizational-units.state.ts
+++ src/app/shared/state/organizational-units.state.ts
@@ -9,48 +9,48 @@ import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { ConstService } from '../services/const.service';
 
 export interface OrganizationalUnitsState {
-    organizationalUnits: IOrganizationUnitList[];
-    loading: boolean;
-    error: string;
+  organizationalUnits: IOrganizationUnitList[];
+  loading: boolean;
+  error: string;
 }
 
 export const organizationalUnitsInitialState: OrganizationalUnitsState = {
-    organizationalUnits: [],
-    loading: false,
-    error: '',
+  organizationalUnits: [],
+  loading: false,
+  error: '',
 };
 
 // Organizational Units Actions
 export const loadOrganizationalUnits = createAction('[OrganizationalUnit] Load Organizational Units');
 export const loadOrganizationalUnitsSuccess = createAction(
-    '[OrganizationalUnit] Load Organizational Units Success',
-    props<{ organizationalUnits: IOrganizationUnitList[] }>(),
+  '[OrganizationalUnit] Load Organizational Units Success',
+  props<{ organizationalUnits: IOrganizationUnitList[] }>(),
 );
 export const loadOrganizationalUnitsFailure = createAction(
-    '[OrganizationalUnit] Load Organizational Units Failure',
-    props<{ error: string }>(),
+  '[OrganizationalUnit] Load Organizational Units Failure',
+  props<{ error: string }>(),
 );
 export const setOrgnizationalUnitremitsFinalized = createAction(
-    '[OrganizationalUnit] Set Organizational Unit Remits Finalized',
-    props<{ code: string; remitsFinalized: boolean }>(),
+  '[OrganizationalUnit] Set Organizational Unit Remits Finalized',
+  props<{ code: string; remitsFinalized: boolean }>(),
 );
 
 // Organizational Units Reducer
 export const organizationalUnitsReducer = createReducer<OrganizationalUnitsState>(
-    organizationalUnitsInitialState,
-    on(loadOrganizationalUnits, (state) => ({ ...state, loading: true })),
-    on(loadOrganizationalUnitsSuccess, (state, { organizationalUnits }) => ({
-        ...state,
-        organizationalUnits,
-        loading: false,
-    })),
-    on(loadOrganizationalUnitsFailure, (state, { error }) => ({ ...state, error, loading: false })),
-    on(setOrgnizationalUnitremitsFinalized, (state, { code, remitsFinalized }) => ({
-        ...state,
-        organizationalUnits: state.organizationalUnits.map((ou) =>
-            ou.code === code ? { ...ou, remitsFinalized } : ou,
-        ),
-    })),
+  organizationalUnitsInitialState,
+  on(loadOrganizationalUnits, (state) => ({ ...state, loading: true })),
+  on(loadOrganizationalUnitsSuccess, (state, { organizationalUnits }) => ({
+    ...state,
+    organizationalUnits,
+    loading: false,
+  })),
+  on(loadOrganizationalUnitsFailure, (state, { error }) => ({ ...state, error, loading: false })),
+  on(setOrgnizationalUnitremitsFinalized, (state, { code, remitsFinalized }) => ({
+    ...state,
+    organizationalUnits: state.organizationalUnits.map((ou) =>
+      ou.code === code ? { ...ou, remitsFinalized } : ou,
+    ),
+  })),
 );
 
 // Organizational Units Selectors
@@ -60,85 +60,85 @@ export const organizationalUnits$ = (state: AppState) => state.organizationalUni
 
 // Use createSelector to create memoized selectors
 export const selectOrganizationalUnitsLoading$ = createSelector(
-    selectOrganizationalUnitsState$,
-    (state) => state.loading,
+  selectOrganizationalUnitsState$,
+  (state) => state.loading,
 );
 export const selectOrganizationalUnits$ = createSelector(
-    selectOrganizationalUnitsState$,
-    (state) => state.organizationalUnits,
+  selectOrganizationalUnitsState$,
+  (state) => state.organizationalUnits,
 );
 // Lets create a selector to return the organization code for a given organizational unit code
 export const selectOrganizationCodeByOrganizationalUnitCode$ = (organizationalUnitCode: string) =>
-    createSelector(selectOrganizationalUnits$, (organizationalUnits) => {
-        const ou = organizationalUnits.find((ou) => ou.code === organizationalUnitCode);
-        return ou ? ou.organizationCode : null;
-    });
+  createSelector(selectOrganizationalUnits$, (organizationalUnits) => {
+    const ou = organizationalUnits.find((ou) => ou.code === organizationalUnitCode);
+    return ou ? ou.organizationCode : null;
+  });
 
 // A selector to return the organization unit codes for a given organizational code
 export const selectOrganizationalUnitCodeByOrganizationCode$ = (organizationCode: string) =>
-    createSelector(selectOrganizationalUnits$, (organizationalUnits) => {
-        const codes = organizationalUnits
-            .filter(ou => ou.organizationCode === organizationCode)
-            .map(ou => ou.code);
-        return codes ? codes : null;
-    });    
+  createSelector(selectOrganizationalUnits$, (organizationalUnits) => {
+    const codes = organizationalUnits
+      .filter(ou => ou.organizationCode === organizationCode)
+      .map(ou => ou.code);
+    return codes ? codes : null;
+  });
 
 // A selector to return the organization unit codes for a given organizational code
 export const selectOrganizationalUnitByOrganizationCode$ = (organizationCode: string) =>
-    createSelector(selectOrganizationalUnits$, (organizationalUnits) => {
-        const codes = organizationalUnits
-            .filter(ou => ou.organizationCode === organizationCode)
-        return codes ? codes : null;
-    });
+  createSelector(selectOrganizationalUnits$, (organizationalUnits) => {
+    const codes = organizationalUnits
+      .filter(ou => ou.organizationCode === organizationCode)
+    return codes ? codes : null;
+  });
 
 // A selector to return the organization unit codes for a given supervisorUnitCode
 export const selectOrganizationalUnitBysupervisorUnitCode$ = (code: string) =>
-createSelector(selectOrganizationalUnits$, (organizationalUnits) => {
+  createSelector(selectOrganizationalUnits$, (organizationalUnits) => {
     const codes = organizationalUnits
-        .filter(ou => ou.supervisorUnitCode === code)
+      .filter(ou => ou.supervisorUnitCode === code)
     return codes ? codes : null;
-});
+  });
 
 // Organizational Units Effects
 export const getOrganizationalUnitsEffect = createEffect(
-    (
-        actions$ = inject(Actions),
-        organizationUnitService = inject(OrganizationalUnitService),
-        constService = inject(ConstService),
-    ) => {
-        return actions$.pipe(
-            ofType(loadOrganizationalUnits),
-            switchMap(() =>
-                organizationUnitService.getAllOrganizationalUnits().pipe(
-                    // tap((orgUnits) => {
-                    //     orgUnits.forEach((ou) => {
-                    //         if (ou.code === '800399' || ou.code === '573529')
-                    //             console.log('AAAAA', constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP.get(ou.code));
-                    //         // console.log(ou.code, constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP[ou.code]);
-                    //         if (constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP[ou.code])
-                    //             console.log('ou FINALIZED', ou);
-                    //     });
-                    // }),
-                    // Use the ORGANIZATION_UNIT_REMITS_FINALIZED_MAP to map the remitsFinalized property
-                    map((organizationalUnits) => {
-                        // console.log(
-                        //     'organizationalUnits',
-                        //     organizationalUnits.map(
-                        //         (ou) => constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP[ou.code] ?? false,
-                        //     ),
-                        // );
-                        return organizationalUnits.map((organizationalUnit) => ({
-                            ...organizationalUnit,
-                            remitsFinalized:
-                                constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP.get(organizationalUnit.code) ??
-                                false,
-                        }));
-                    }),
-                    map((organizationalUnits) => loadOrganizationalUnitsSuccess({ organizationalUnits })),
-                    catchError((error) => of(loadOrganizationalUnitsFailure({ error: error.message }))),
-                ),
-            ),
-        );
-    },
-    { functional: true },
+  (
+    actions$ = inject(Actions),
+    organizationUnitService = inject(OrganizationalUnitService),
+    constService = inject(ConstService),
+  ) => {
+    return actions$.pipe(
+      ofType(loadOrganizationalUnits),
+      switchMap(() =>
+        organizationUnitService.getAllOrganizationalUnits().pipe(
+          // tap((orgUnits) => {
+          //     orgUnits.forEach((ou) => {
+          //         if (ou.code === '800399' || ou.code === '573529')
+          //             console.log('AAAAA', constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP.get(ou.code));
+          //         // console.log(ou.code, constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP[ou.code]);
+          //         if (constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP[ou.code])
+          //             console.log('ou FINALIZED', ou);
+          //     });
+          // }),
+          // Use the ORGANIZATION_UNIT_REMITS_FINALIZED_MAP to map the remitsFinalized property
+          // map((organizationalUnits) => {
+          //     // console.log(
+          //     //     'organizationalUnits',
+          //     //     organizationalUnits.map(
+          //     //         (ou) => constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP[ou.code] ?? false,
+          //     //     ),
+          //     // );
+          //     return organizationalUnits.map((organizationalUnit) => ({
+          //         ...organizationalUnit,
+          //         remitsFinalized:
+          //             constService.ORGANIZATION_UNIT_REMITS_FINALIZED_MAP.get(organizationalUnit.code) ??
+          //             false,
+          //     }));
+          // }),
+          map((organizationalUnits) => loadOrganizationalUnitsSuccess({ organizationalUnits })),
+          catchError((error) => of(loadOrganizationalUnitsFailure({ error: error.message }))),
+        ),
+      ),
+    );
+  },
+  { functional: true },
 );
