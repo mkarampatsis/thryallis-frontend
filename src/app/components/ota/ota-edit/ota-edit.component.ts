e284b971010d33866048a9d18158b5ea9984d2ba
mkarampatsis <marka@central.ntua.gr>
2025-11-11 12:24:12 +0200

    OTA

--- src/app/components/ota/ota-edit/ota-edit.component.ts
+++ src/app/components/ota/ota-edit/ota-edit.component.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-ota-edit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './ota-edit.component.html',
   styleUrl: './ota-edit.component.css'
 })
-export class OtaEditComponent {
+export class OtaEditComponent{
+  otaForm!: FormGroup;
 
+  organizations = [
+    { name: 'Υπουργείο Εσωτερικών', code: 'YPI-001' },
+    { name: 'Περιφέρεια Αττικής', code: 'PER-002' },
+    { name: 'Δήμος Αθηναίων', code: 'DIM-003' }
+  ];
+
+  remitCompetences = [
+    'Περιφέρειες',
+    'Δήμοι',
+    'Νησιωτικοί Δήμοι',
+    'Ορεινοί Δήμοι',
+    'Μητροπολιτικές Αρμοδιότητες'
+  ];
+
+  remitTypes = [
+    'Επιτελική',
+    'Εκτελεστική',
+    'Υποστηρικτική',
+    'Ελεγκτική',
+    'Παρακολούθησης αποτελεσματικής πολιτικής και αξιολόγησης αποτελεσμάτων'
+  ];
+
+  form = new FormGroup({
+    remitContent: new FormControl('', Validators.required),
+    organization: new FormControl('', Validators.required),
+    organizationCode: new FormControl('', Validators.required),
+    remitType: new FormGroup({
+      remitContent: new FormControl('', Validators.required),
+      organizationRemitCompetence: new FormControl('', Validators.required),
+      remitType: new FormControl('', Validators.required),
+      legalProvision: new FormControl('', Validators.required)
+    }),
+    legalProvision: new FormControl('', Validators.required),
+    circularInstructions: new FormControl('', Validators.required),
+    publicPolicyAgency: new FormGroup({
+      organization: new FormControl('', Validators.required),
+      organizationCode: new FormControl('', Validators.required),
+      organizationalUnit: new FormControl('', Validators.required),
+      organizationalUnitCode: new FormControl('', Validators.required)
+    })
+  })
+
+  // CRUD Methods
+  onCreate() {
+    if (this.otaForm.valid) {
+      console.log('Create:', this.otaForm.getRawValue());
+    }
+  }
+
+  onUpdate() {
+    console.log('Update:', this.otaForm.getRawValue());
+  }
+
+  onDelete() {
+    console.log('Delete current entry');
+  }
+
+  onClear() {
+    this.otaForm.reset();
+  }
 }
